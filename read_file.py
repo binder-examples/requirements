de46e552ff16187287843ddd25e7044392256dfd
Joshua Jackson (He/Him) <122300334+Majikjack620@users.noreply.github.com>
2023-06-15 13:15:41 -0500

    Update read_file.py

--- read_file.py
+++ read_file.py
@@ -1,11 +1,21 @@
 with open('dependent.txt', 'r') as file:
-  for line in file:
-    listings = list(line.split())
-    length = len(listings)
+  if getsize(file) == 0:
+    print(":heavy_check_mark: All dependencies up to date :heavy_check_mark:")
+  else:
+    print("### :x: Outdated/Missing Dependencies List :x:")
+    for line in file:
+      listings = list(line.split())
+      length = len(listings)
     
-    for i in range(length):
-      if i == length-1:
-        print(listings[i], end=" | \n")
-      else:
-        print(listings[i], end=" | ")
-      
+      for i in range(length):
+        if i == length-1:
+          print(listings[i], end=" | \n")
+        else:
+          print(listings[i], end=" | ")
+
+    print("##### Update these dependencies:")
+    print("""- Ensure that ALL dependencies in the ***requirements.txt*** file are connected to their version with 
+    ***>=*** and not ***==*** (i.e. ***pandas>=1.5.1***)""")
+    print("- Run the following command within the folder the ***requirements.txt*** file resides")
+    print("  - ***pip install -r requirements.txt -U***")
+file.close()
